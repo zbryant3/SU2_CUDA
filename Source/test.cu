7fb73d431e8d2a0725be3666bdbc012374c8c95d
zbryant3 <zbryant3@gmail.com>
2019-08-09 11:08:28 -0400

    Testing matrix multiplication

--- Source/test.cu
+++ Source/test.cu
@@ -7,20 +7,31 @@
 
 using namespace std;
 
-__global__ void foo(){
-  bach::complex<double> c(66,33);
-  bach::complex<double> b(1,2);
-  bach::complex<double> a(0,0);
-  a = b + c;
-  bach::print(a);
+
+__device__ void MaMult(bach::complex<double> *m1, bach::complex<double> *m2, bach::complex<double> *r){
+
+  bach::complex<double> neg = bach::complex<double> (-1,-1);
+
+        r[0] = m1[0]*m2[0] + m1[1]*m2[2];
+        r[1] = m1[0]*m2[1] + m1[1]*m2[3];
+        r[2] = neg*bach::conj(r[1]);
+        r[3] = bach::conj(r[0]);
 
 };
 
+__global__ void func(){
+  bach::complex<double> m (1,9);
+  bach::complex<double> n (2, 4);
+  m = m / 6 ;
+  bach::print(m);
+}
+
 
 int main(){
-    foo<<<1,1>>>();
-    cudaDeviceSynchronize();
+
+        func<<<1,1>>>();
+        cudaDeviceSynchronize();
 
 
-    return 0;
+        return 0;
 }
